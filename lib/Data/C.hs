9bd1a0e8713a556faa4469f3513953004ae4a82d
Michelle Schifferstein <m.schifferstein@student.tudelft.nl>
2022-05-17 14:32:15 +0200

    change C to data type to avoid type synonym instance problems
    C as type synonym is stored in the archive (C_Type). Minimal changes have been applied to convert C to a data type. Haskell code now compiles without errors.

--- lib/Data/C.hs
+++ lib/Data/C.hs
@@ -1,36 +1,40 @@
-{-# LANGUAGE TypeSynonymInstances #-}
 module Data.C where
+
+import Data.MonadTrans
 
 data Action m = Atom (m (Action m))
               | Fork (Action m) (Action m)
               | Stop
 
-type C m a = (a -> Action m) -> Action m
+data C m a = Conc{act :: (a -> Action m) -> Action m}
 
 instance (Monad m) => Functor (C m) where
     fmap f x = x >>= pure . f
 
 instance (Monad m) => Applicative (C m) where
-    pure = \ x c -> c x
+    pure = \ x -> Conc (\ c -> c x)
     mf <*> ma = mf >>= \ f -> ma >>= \ a -> pure (f a)
 
 instance (Monad m) => Monad (C m) where
-    (>>=) = \ f k c -> f (\ a -> k a c)
+    f >>= k = Conc (\ c -> act f (\ a -> act (k a) c))
 
 atom :: Monad m => m a -> C m a
-atom m = \ c -> Atom (fmap c m)
+atom m = Conc (\ c -> Atom (fmap c m))
 
 action :: Monad m => C m a -> Action m
-action m = m (\ _ -> Stop)
+action m = act m (\ _ -> Stop)
 
 stop :: Monad m => C m a
-stop = \ _ -> Stop
+stop = Conc (\ _ -> Stop)
 
 par :: Monad m => C m a -> C m a -> C m a
-par m1 m2 = \ c -> Fork (m1 c) (m2 c)
+par m1 m2 = Conc (\ c -> Fork (act m1 c) (act m2 c))
 
 fork :: Monad m => C m a -> C m ()
-fork m = \ c -> Fork (action m) (c ())
+fork m = Conc (\ c -> Fork (action m) (c ()))
+
+instance MonadTrans C where
+    lift = atom
 
 round_robin :: Monad m => [Action m] -> m ()
 round_robin [] = return ()
--- src/Doc/Log.md
+++ src/Doc/Log.md
@@ -1,5 +1,5 @@
 # Issue 1: instance declarations with C #
-In `C.hs`, the instance declarations are not allowed unless `{-# LANGUAGE TypeSynonymInstances #-}` is added as a flag.
+In `C.hs`, the instance declarations are not allowed unless `{-# LANGUAGE TypeSynonymInstances #-}` is added as a flag (at top of file).
 
 Next, it complains that `Functor (C m)` etc. are not allowed because `C` takes two arguments, while only one is given.
 
@@ -14,6 +14,18 @@ I.e. they are defined as `newtype` rather than `type`. So perhaps we could do:
 ```
 newtype C m a = C { getC :: (a -> Action m) -> Action m }
 ```
+In that case the pragma from above isn't needed anymore.
+
+However, `Agda2hs` does not support `newtype` definitions at present, so we need to represent `C` as a data type instead:
+
+```
+record C (m : Set → Set) (a : Set) : Set where
+    constructor Conc
+    field
+        act : (a → Action m) → Action m
+open C public
+```
+
 
 # Issue 2: solving instance constraints for CWriter #
 
