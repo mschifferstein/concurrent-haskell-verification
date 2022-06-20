9291f8c844b782ed603fef9f86b3d8c7a605143a
Michelle Schifferstein <m.schifferstein@student.tudelft.nl>
2022-06-20 16:37:51 +0200

    final agda2hs translation and demo update

--- lib/Programs.hs
+++ lib/Programs.hs
@@ -19,3 +19,54 @@ natToMyNat n | n <= 0 = Zero
 fuel :: MyNat
 fuel = natToMyNat 9223372036854775800
 
+mVarDeadlock :: Bool
+mVarDeadlock
+  = runIOs
+      (run
+         (newEmptyMVar >>=
+            \ a ->
+              newEmptyMVar >>=
+                \ b ->
+                  fork (takeMVar a fuel >> writeMVar b 1 fuel) >>
+                    (takeMVar b fuel >> writeMVar a 2 fuel))
+         (natToMyNat 100000))
+
+mVarNoDeadlock :: Bool
+mVarNoDeadlock
+  = runIOs
+      (run
+         (newEmptyMVar >>=
+            \ a ->
+              newEmptyMVar >>=
+                \ b ->
+                  fork (takeMVar a fuel >> writeMVar b 1 fuel) >>
+                    (writeMVar a 2 fuel >> takeMVar b fuel))
+         (natToMyNat 100000))
+
+simpleDeadlock :: Bool
+simpleDeadlock
+  = runIOs
+      (run (newEmptyMVar >>= \ a -> takeMVar a fuel) (natToMyNat 100000))
+
+simple :: Bool
+simple
+  = runIOs
+      (run (newEmptyMVar >>= \ var -> writeMVar var 5 fuel)
+         (natToMyNat 100000))
+
+failDetect :: Bool
+failDetect
+  = runIOs
+      (run
+         (newEmptyMVar >>=
+            \ a ->
+              newMVar 0 >>=
+                \ mutex ->
+                  fork
+                    (takeMVar mutex fuel >>
+                       (writeMVar a 2 fuel >> writeMVar mutex 0 fuel))
+                    >>
+                    (takeMVar mutex fuel >>
+                       (takeMVar a fuel >> writeMVar mutex 0 fuel)))
+         (natToMyNat 100000))
+
